7d5b3a047b629988ac8108610edbbce8c6061b72
orson <orson@techheathen.com>
2017-09-02 16:19:41 +0300

    minor style changes

--- types.h
+++ types.h
@@ -60,24 +60,20 @@ typedef struct _orbit
   unsigned int rev_number;    // Number of revolutions at epoch
   // Time
   double julepoch, GSTo;
-  // Standard orbital terms
-  double a, altapoR, altperR, aycof, C1, C4, C5, con41, cosi, d2, d3, d4, delMo, eta,
-         mdot, nodecf, nodedot, omegaprime, omgcof, sinMo, sini, t2cof, t3cof, t4cof,
-         t5cof, x1mth2, x7thm1, xlcof, xmcof;
-  // Deep space terms
-  double e3,  ee2,  peo,  pgho, pho,  pinco, plo, se2,
-         se3, sgh2, sgh3, sgh4, sh2,  sh3,   si2, si3,
-         sl2, sl3,  sl4,  xgh2, xgh3, xgh4,  xh2, xh3,
-         xi2, xi3,  xl2,  xl3,  xl4,  zmol,  zmos;
-  // Resonant terms
-  double d2201, d2211, d3210, d3222 ,
-  d4410, d4422, d5220, d5232,
-  d5421, d5433, dedt,  didt,
-  dmdt,         dnodt, domdt ,
-  del1,  del2,  del3,  xfact,
-  xlamo, xli,   xni;
   // Flags
   bool isdeepspace, islowperigee;
+  // Standard orbital terms
+  double a, altapoR, altperR, aycof, C1, C4, C5, con41, cosi, d2, d3, d4,
+         delMo, eta,   mdot,  nodecf, nodedot, omegaprime, omgcof, sinMo, sini,
+         t2cof, t3cof, t4cof, t5cof,  x1mth2,  x7thm1,     xlcof,  xmcof;
+  // Deep space terms
+  double e3,  ee2,  peo, pgho, pho,  pinco, plo, se2,  se3,  sgh2, sgh3, sgh4,
+         sh2, sh3,  si2, si3,  sl2,  sl3,   sl4, xgh2, xgh3, xgh4, xh2,  xh3,
+         xi2, xi3,  xl2, xl3,  xl4,  zmol,  zmos;
+  // Resonant terms
+  double d2201, d2211, d3210, d3222, d4410, d4422, d5220, d5232, d5421, d5433,
+         dedt,  didt,  dmdt,  dnodt, domdt, del1,  del2,  del3,  xfact, xlamo,
+         xli,   xni;
 } orbit;
 
 void print_orbit(orbit* sat, char* caption);
