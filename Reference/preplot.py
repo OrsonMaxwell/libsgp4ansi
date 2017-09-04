db42f552b28ce80d41c7175120da26c3173ee534
Orson <orson@techheathen.com>
2017-09-04 21:49:01 +0300

    Failure to produce reference tests - cleaning up

--- Reference/preplot.py
+++ Reference/preplot.py
@@ -23,18 +23,18 @@ for i, line in enumerate(data1):
     if asfc_vals[0] not in dotsv.keys():
       dotsv[asfc_vals[0]] = []
     diff = max(abs(asfc_vals[1] - impr_vals[1]), abs(asfc_vals[2] - impr_vals[2]), abs(asfc_vals[3] - impr_vals[3]))
-    if diff != 0:
+    if (diff != 0):
       dotsp[asfc_vals[0]].append(diff)
-    diff = max(abs(asfc_vals[4] - impr_vals[4]), abs(asfc_vals[5] - impr_vals[5]), abs(asfc_vals[6] - impr_vals[6]))
-    if diff != 0:
-      dotsv[asfc_vals[0]].append(diff)
+    #diff = max(abs(asfc_vals[4] - impr_vals[4]), abs(asfc_vals[5] - impr_vals[5]), abs(asfc_vals[6] - impr_vals[6]))
+    #if (diff != 0):
+    #  dotsv[asfc_vals[0]].append(diff)
     
 with open('{}-{}pos.csv'.format(dataset1, dataset2), 'w') as f:
   for timestamp in dotsp.keys():
     for diff in dotsp[timestamp]:
       f.write('{},{}\n'.format(timestamp, diff * 1000))
 
-with open('{}-{}vel.csv'.format(dataset1, dataset2), 'w') as f:
-  for timestamp in dotsp.keys():
-    for diff in dotsv[timestamp]:
-      f.write('{},{}\n'.format(timestamp, diff * 1000))
+#with open('{}-{}vel.csv'.format(dataset1, dataset2), 'w') as f:
+#  for timestamp in dotsp.keys():
+#    for diff in dotsv[timestamp]:
+#      f.write('{},{}\n'.format(timestamp, diff * 1000))
