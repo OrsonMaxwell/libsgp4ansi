3e4f9154661ac73f68621b47d9eb86b766cec582
Orson <orson@techheathen.com>
2017-09-06 00:59:34 +0300

    To prev commit

--- Reference/plot.py
+++ Reference/plot.py
@@ -9,46 +9,63 @@ with open(sys.argv[1] + '/a721.out') as f:
   ref = f.readlines()
 with open(sys.argv[1] + '/i72.out') as f:
   diff1 = f.readlines()
+with open(sys.argv[1] + '/ansi.out') as f:
+  diff2 = f.readlines()
 
 current_sat = ''
 orbital_period = 0.0
-diff = 0.0
-dotsp = {}
+delta1 = 0.0
+delta2 = 0.0
+dotsp1 = {}
+dotsp2 = {}
 
 for i, line in enumerate(ref):
   m = re.fullmatch('([0-9]{1,5})\ \(([0-9\.]+)\)', line.strip())
   if m != None:
-    if len(dotsp.keys()) > 0:
-      dotsp[current_sat][0].append(orbital_period)
-      dotsp[current_sat][1].append(1000 * diff) # get back to meters
-      print('[{0}] Sat {1}, period: {2:8.3f}, '.format(i, current_sat, orbital_period), end='')
-      print('max deviation: {0:10.9f}'.format(1000 * diff))
+    if len(dotsp1.keys()) > 0:
+      dotsp1[current_sat][0].append(orbital_period)
+      dotsp1[current_sat][1].append(1000 * delta1) # get back to meters
+      dotsp2[current_sat][0].append(orbital_period)
+      dotsp2[current_sat][1].append(1000 * delta2)
+      print('[{0}] Sat {1}, per:{2:8.3f}, '.format(i, current_sat, orbital_period), end='')
+      print('d1: {0:10.9f}, d2: {1:10.9f}'.format(1000 * delta1, 1000 * delta2))
     current_sat = m.group(1)
     orbital_period = float(m.group(2))
-    dotsp[current_sat] = ([], [])
-    diff = 0.0
+    dotsp1[current_sat] = ([], [])
+    dotsp2[current_sat] = ([], [])
+    delta1 = 0.0
+    delta2 = 0.0
   else:
     ref_vals = list(map(float, line.split()))
     diff1_vals = list(map(float, diff1[i].split()))
-    cur_line_max_diff = max(abs(ref_vals[1] - diff1_vals[1]), abs(ref_vals[2] - diff1_vals[2]), abs(ref_vals[3] - diff1_vals[3]))
-    if diff < cur_line_max_diff:
-      diff = cur_line_max_diff
+    diff2_vals = list(map(float, diff2[i].split()))
+    tempdelta1 = max(abs(ref_vals[1] - diff1_vals[1]), abs(ref_vals[2] - diff1_vals[2]), abs(ref_vals[3] - diff1_vals[3]))
+    if delta1 < tempdelta1:
+      delta1 = tempdelta1
+    tempdelta2 = max(abs(ref_vals[1] - diff2_vals[1]), abs(ref_vals[2] - diff2_vals[2]), abs(ref_vals[3] - diff2_vals[3]))
+    if delta2 < tempdelta2:
+      delta2 = tempdelta2
 
-dotsp[current_sat][0].append(orbital_period)
-dotsp[current_sat][1].append(1000 * diff) # get back to meters
-print('[   EOF] Sat {0}, period: {1:8.3f}, '.format(current_sat, orbital_period), end='')
-print('max deviation: {0:10.9f}'.format(1000 * diff))
-      
-print('Loaded {} data points'.format(len(dotsp.keys())))
+# Repeat for last sat
+dotsp1[current_sat][0].append(orbital_period)
+dotsp1[current_sat][1].append(1000 * delta1) # get back to meters
+dotsp2[current_sat][0].append(orbital_period)
+dotsp2[current_sat][1].append(1000 * delta2)
+print('[   EOF] Sat {0}, per:{1:8.3f}, '.format(current_sat, orbital_period), end='')
+print('d1: {0:10.9f}, d2: {1:10.9f}'.format(1000 * delta1, 1000 * delta2))
+print('Loaded {} data points per set'.format(len(dotsp1.keys())))
 
+# Plotting
 xmin = 0
 xmax = 2000
 xticksmin = range(xmin, xmax + 1, 100)
 xticksmaj = range(xmin, xmax + 1, 500)
     
 plt.subplot(111)
-for st in dotsp.keys():  
-  plt.plot(dotsp[st][0], dotsp[st][1], c='b', marker='D', ls="", mew=0.0, markersize=3, alpha=0.55)
+for st in dotsp1.keys():  
+  plt.plot(dotsp1[st][0], dotsp1[st][1], c='b', marker='D', ls="", mew=0.0, markersize=3, alpha=0.55)
+for st in dotsp2.keys():  
+  plt.plot(dotsp2[st][0], dotsp2[st][1], c='r', marker='D', ls="", mew=0.0, markersize=3, alpha=0.55)
 plt.subplot(111).set_xlim(xmin, xmax)
 plt.subplot(111).set_xticks(xticksmin, True);
 plt.subplot(111).set_xticks(xticksmaj, False);
@@ -56,7 +73,7 @@ plt.subplot(111).set_yscale("log")
 plt.subplot(111).autoscale(True, 'y', None)
 plt.subplot(111).grid(which='major', axis='both', ls='dashed', alpha=0.7)
 plt.subplot(111).grid(which='minor', axis='x', ls='dotted', alpha=0.3)
-plt.title('Max difference in ephemerides, m: STR#3 vs AIAA (blue)')
+plt.title('Max difference in ephemerides: STR#3 vs AIAA (blue), STR#3 vs ANSI (red)')
 plt.xlabel('Orbital period, min')
 plt.ylabel('Difference, m')
 plt.tight_layout(pad=0.0, w_pad=0.1, h_pad=0.1)
