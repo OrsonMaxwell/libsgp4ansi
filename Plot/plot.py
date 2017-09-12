422c5dc8a42bcfce54fb826c924d2a47cf826044
orson <orson@techheathen.com>
2017-09-12 10:48:37 +0300

    AIAA paper plots complete.

--- Plot/plot.py
+++ Plot/plot.py
@@ -184,9 +184,67 @@ for i, line in enumerate(ansi_ver_file):
 
 print('')
 
+# Plotting everything #########################################################
 
-# Plot #3 - Solving Kepler's Equation for satellite 23333 #####################
+print('Plotting data - please wait...')
 
+# Max difference in ephemerides between maths #################################
+fig, ax = plt.subplots(1, 1)
+fig.canvas.set_window_title('Max. diff in ephems') 
+
+xmin = 0
+xmax = 2000
+xticksmin = range(xmin, xmax + 1, 100)
+xticksmaj = range(xmin, xmax + 1, 500)
+
+for sat in list(aiaa_sats.keys()):
+  ax.plot(aiaa_sats[sat][0], fullcat_points[sat], c='b', marker='D', ls='', mew=0.0, markersize=3, alpha=0.55)
+  
+ax.set_xlim(xmin, xmax)
+ax.set_xticks(xticksmin, True);
+ax.set_xticks(xticksmaj, False);
+ax.set_yscale('log')
+ax.autoscale(True, 'y', None)
+ax.grid(which='major', axis='both', ls='dashed', alpha=0.7)
+ax.grid(which='minor', axis='x', ls='dotted', alpha=0.3)
+ax.set_title('Max difference in ephemerides: AIAA (zero) vs ANSI (blue)')
+ax.set_xlabel('Orbital period, min')
+ax.set_ylabel('Difference, m')
+fig.set_tight_layout({'pad':0.0, 'w_pad':0.1, 'h_pad':0.1})
+
+# Runtime difference between static reference and library code ################
+if (not dry_run):
+  tic = 2
+  tansic = 6
+  tit = 80
+  tansit = 49
+
+  fig, (axtt, axtc) = plt.subplots(2, 1)
+  fig.canvas.set_window_title('Timing results') 
+  
+  axtt.barh([1.9], [tdiff_it.total_seconds()], height=0.5, color='g')
+  axtt.barh([1.1], [tdiff_ansit.total_seconds()], height=0.5, color='b')
+  axtt.autoscale(True, 'x', None)
+  axtt.set_yticks([], True)
+  axtt.set_yticks([], False)
+  axtt.set_title('Timing')
+  axtt.set_ylabel('Synth. test')
+  axtt.text(tdiff_it.total_seconds() / 2, 1.85, 'AIAA', ha='center', color='w', weight='bold')
+  axtt.text(tdiff_ansit.total_seconds() / 2, 1.05, 'ANSI', ha='center', color='w', weight='bold')
+  
+  axtc.barh([1.9], [tdiff_ic.total_seconds()], height=0.5, color='g')
+  axtc.barh([1.1], [tdiff_ansic.total_seconds()], height=0.5, color='b')
+  axtc.autoscale(True, 'x', None)
+  axtc.set_yticks([], True)
+  axtc.set_yticks([], False)
+  axtc.set_xlabel('Run time, s')
+  axtc.set_ylabel('Real run')
+  axtc.text(tdiff_ic.total_seconds() / 2, 1.85, 'AIAA', ha='center', color='w', weight='bold')
+  axtc.text(tdiff_ansic.total_seconds() / 2, 1.05, 'ANSI', ha='center', color='w', weight='bold')
+  
+  fig.set_tight_layout({'pad':0.0, 'w_pad':0.1, 'h_pad':0.1})
+
+# Solving Kepler's Equation for satellite 23333 ###############################
 aiaa_23333_t = []
 aiaa_23333_i = []
 ansi_23333_i = []
@@ -200,7 +258,48 @@ for time in aiaa_ver_sats['23333'][2].keys():
   ansi_23333_i.append(ansi_ver_sats['23333'][2][time][5])
   ansi_23333_m.append(ansi_ver_sats['23333'][2][time][9])
 
-# Plot #4 - Position components for satellite 23599 ###########################
+fig, (axi, axm) = plt.subplots(2, 1, sharex=True)
+fig.canvas.set_window_title('Kepler\'s Equation for 23333') 
+
+xmin = 240
+xmax = 720
+xticksmin = range(xmin, xmax + 1, 60)
+xticksmaj = range(xmin, xmax + 1, 120)
+
+y2min = 0
+y2max = 360
+y2ticksmin = [30, 60, 210, 240, 300, 330]
+y2ticksmaj = [90, 180, 270, 360]
+
+axi.plot(aiaa_23333_t, aiaa_23333_i, c='k', marker='', ls=':')
+axi.plot(aiaa_23333_t, ansi_23333_i, c='b', marker='', ls='-', alpha=0.6)
+
+axm.plot(aiaa_23333_t, aiaa_23333_m, c='k', marker='', ls=':')
+axm.plot(aiaa_23333_t, ansi_23333_m, c='g', marker='', ls='-', alpha=0.6)
+
+axi.set_xlim(xmin, xmax)
+axi.set_xticks(xticksmin, True);
+axi.set_xticks(xticksmaj, False);
+axi.autoscale(True, 'y', None)
+axi.grid(which='major', axis='both', ls='dashed', alpha=0.7)
+axi.grid(which='minor', axis='x', ls='dotted', alpha=0.3)
+axi.set_title('Kepler\'s for 23333, AIAA (black dashed) vs ANSI (color)')
+axi.set_ylabel('Inclination, deg')
+
+axm.set_xticks(xticksmin, True);
+axm.set_xticks(xticksmaj, False);
+axm.set_ylim(y2min, y2max)
+axm.set_yticks(y2ticksmin, True);
+axm.set_yticks(y2ticksmaj, False);
+axm.grid(which='major', axis='both', ls='dashed', alpha=0.7)
+axm.grid(which='minor', axis='x', ls='dotted', alpha=0.3)
+axm.set_xlabel('Time from epoch, min')
+axm.set_ylabel('Mean anomaly, deg')
+fig.set_tight_layout({'pad':0.0, 'w_pad':0.1, 'h_pad':0.1})
+  
+# Lunar-solar modifications for Satellite 23599 ###############################
+aiaa_argper = []
+ansi_argper = []
 
 aiaa_pos23599_x1 = []
 aiaa_pos23599_yx = []
@@ -213,131 +312,103 @@ ansi_pos23599_yz = []
 
 for time in aiaa_ver_sats['23599'][2].keys():
   aiaa_pos23599_x1.append(time)
+  aiaa_argper.append(aiaa_ver_sats['23599'][2][time][7])
+  ansi_argper.append(ansi_ver_sats['23599'][2][time][7])
   aiaa_pos23599_yx.append(aiaa_ver_sats['23599'][2][time][0])
   aiaa_pos23599_yy.append(aiaa_ver_sats['23599'][2][time][1])
   aiaa_pos23599_yz.append(aiaa_ver_sats['23599'][2][time][2])
   ansi_pos23599_yx.append(ansi_ver_sats['23599'][2][time][0])
   ansi_pos23599_yy.append(ansi_ver_sats['23599'][2][time][1])
   ansi_pos23599_yz.append(ansi_ver_sats['23599'][2][time][2])
-  
-# Plotting everything #########################################################
 
-# Plotting
-print('Plotting data - please wait...')
-
-# Max difference in ephemerides between maths
-fig = plt.figure(1)
-fig.canvas.set_window_title('Max. diff in ephems') 
-
-xmin = 0
-xmax = 2000
-xticksmin = range(xmin, xmax + 1, 100)
-xticksmaj = range(xmin, xmax + 1, 500)
-
-plt.subplot(111)
-for sat in list(aiaa_sats.keys()):
-  plt.plot(aiaa_sats[sat][0], fullcat_points[sat], c='b', marker='D', ls='', mew=0.0, markersize=3, alpha=0.55)
-  
-plt.subplot(111).set_xlim(xmin, xmax)
-plt.subplot(111).set_xticks(xticksmin, True);
-plt.subplot(111).set_xticks(xticksmaj, False);
-plt.subplot(111).set_yscale('log')
-plt.subplot(111).autoscale(True, 'y', None)
-plt.subplot(111).grid(which='major', axis='both', ls='dashed', alpha=0.7)
-plt.subplot(111).grid(which='minor', axis='x', ls='dotted', alpha=0.3)
-plt.title('Max difference in ephemerides: AIAA (zero) vs ANSI (blue)')
-plt.xlabel('Orbital period, min')
-plt.ylabel('Difference, m')
-plt.tight_layout(pad=0.0, w_pad=0.1, h_pad=0.1)
-
-if (not dry_run):
-# Runtime difference between static reference and library code
-  fig = plt.figure(2)
-  fig.canvas.set_window_title('Timing results') 
-  plt.subplot(111)
-  plt.bar([1,2], [tdiff_it.total_seconds(), tdiff_ansit.total_seconds()])
-  plt.subplot(111).autoscale(True, 'y', None)
-  plt.title('Timing')
-  plt.xlabel('')
-  plt.ylabel('Run time, s')
-  plt.tight_layout(pad=0.0, w_pad=0.1, h_pad=0.1)
-
-# Solving Kepler's Equation for satellite 23333
-fig = plt.figure(3)
-fig.canvas.set_window_title('Solving Kepler\'s Equation for satellite 23333') 
-
-xmin = int(min(aiaa_23333_t))
-xmax = int(max(aiaa_23333_t))
-xticksmin = range(xmin, xmax + 1, 50)
-xticksmaj = range(xmin, xmax + 1, 200)
-
-y1min = 27.5
-y1max = 27.6
-y1ticksmaj = [27.51, 27.52, 27.53, 27.54, 27.55, 27.56, 27.57, 27.58, 27.59]
-
-y2min = 0
-y2max = 360
-y2ticksmin = [30, 60, 210, 240, 300, 330]
-y2ticksmaj = [90, 180, 270, 360]
-
-plt.subplot(211)
-plt.plot(aiaa_23333_t, aiaa_23333_i, c='k', marker='', ls=':')
-plt.plot(aiaa_23333_t, ansi_23333_i, c='b', marker='', ls='-', alpha=0.6)
-plt.subplot(212)
-plt.plot(aiaa_23333_t, aiaa_23333_m, c='k', marker='', ls=':')
-plt.plot(aiaa_23333_t, ansi_23333_m, c='g', marker='', ls='-', alpha=0.6)
-
-plt.subplot(211).set_xlim(xmin, xmax)
-plt.subplot(211).set_xticks(xticksmin, True);
-plt.subplot(211).set_xticks(xticksmaj, False);
-# plt.subplot(211).set_ylim(y1min, y1max)
-# plt.subplot(211).set_yticks(y1ticksmaj, False);
-plt.subplot(211).autoscale(True, 'y', None)
-plt.subplot(211).grid(which='major', axis='both', ls='dashed', alpha=0.7)
-plt.subplot(211).grid(which='minor', axis='x', ls='dotted', alpha=0.3)
-plt.title('Kepler\'s for 23333, AIAA (black dashed) vs ANSI (color)')
-plt.xlabel('Time from epoch, min')
-plt.ylabel('Inclination, deg, km')
-plt.subplot(212).set_xlim(xmin, xmax)
-plt.subplot(212).set_xticks(xticksmin, True);
-plt.subplot(212).set_xticks(xticksmaj, False);
-plt.subplot(212).set_ylim(y2min, y2max)
-plt.subplot(212).set_yticks(y2ticksmin, True);
-plt.subplot(212).set_yticks(y2ticksmaj, False);
-plt.subplot(212).grid(which='major', axis='both', ls='dashed', alpha=0.7)
-plt.subplot(212).grid(which='minor', axis='x', ls='dotted', alpha=0.3)
-plt.title('Kepler\'s for 23333, AIAA (black dashed) vs ANSI (color)')
-plt.xlabel('Time from epoch, min')
-plt.ylabel('Mean anomaly, deg, km')
-plt.tight_layout(pad=0.0, w_pad=0.1, h_pad=0.1)
-
-  
-# Lunar-solar modifications for Satellite 23599
-fig = plt.figure(4)
-fig.canvas.set_window_title('Lunar-solar modifications for Satellite 23599') 
+fig, (axa, axp) = plt.subplots(2, 1, sharex=True)
+fig.canvas.set_window_title('L-S mods for 23599') 
 
 xmin = int(min(aiaa_pos23599_x1))
 xmax = int(max(aiaa_pos23599_x1))
 xticksmin = range(xmin, xmax + 1, 20)
 xticksmaj = range(xmin, xmax + 1, 60)
 
-plt.plot(aiaa_pos23599_x1, aiaa_pos23599_yx, c='k', marker='', ls=':')
-plt.plot(aiaa_pos23599_x1, aiaa_pos23599_yy, c='k', marker='', ls=':')
-plt.plot(aiaa_pos23599_x1, aiaa_pos23599_yz, c='k', marker='', ls=':')
+axa.plot(aiaa_pos23599_x1, aiaa_argper, c='k', marker='', ls=':')
+axa.plot(aiaa_pos23599_x1, ansi_argper, c='r', marker='', ls='-', alpha=0.6)
 
-plt.plot(aiaa_pos23599_x1, ansi_pos23599_yx, c='b', marker='', ls='-', alpha=0.6)
-plt.plot(aiaa_pos23599_x1, ansi_pos23599_yy, c='g', marker='', ls='-', alpha=0.6)
-plt.plot(aiaa_pos23599_x1, ansi_pos23599_yz, c='m', marker='', ls='-', alpha=0.6)
+axa.set_xlim(xmin, xmax)
+axa.set_xticks(xticksmin, True);
+axa.set_xticks(xticksmaj, False);
+axa.autoscale(True, 'y', None)
+axa.grid(which='major', axis='both', ls='dashed', alpha=0.7)
+axa.grid(which='minor', axis='x', ls='dotted', alpha=0.3)
+axa.set_title('L-S mod. for 23599, AIAA (black dashed) vs ANSI (color)')
+axa.set_ylabel('Argument of perigee, deg')
 
-plt.subplot(111).set_xlim(xmin, xmax)
-plt.subplot(111).set_xticks(xticksmin, True);
-plt.subplot(111).set_xticks(xticksmaj, False);
-plt.subplot(111).autoscale(True, 'y', None)
-plt.subplot(111).grid(which='major', axis='both', ls='dashed', alpha=0.7)
-plt.subplot(111).grid(which='minor', axis='x', ls='dotted', alpha=0.3)
-plt.title('L-S mod. for 23599, AIAA (black dashed) vs ANSI (color)')
-plt.xlabel('Time from epoch, min')
-plt.ylabel('Position components, km')
-plt.tight_layout(pad=0.0, w_pad=0.1, h_pad=0.1)
+axp.plot(aiaa_pos23599_x1, aiaa_pos23599_yx, c='k', marker='', ls=':')
+axp.plot(aiaa_pos23599_x1, aiaa_pos23599_yy, c='k', marker='', ls=':')
+axp.plot(aiaa_pos23599_x1, aiaa_pos23599_yz, c='k', marker='', ls=':')
+
+axp.plot(aiaa_pos23599_x1, ansi_pos23599_yx, c='b', marker='', ls='-', alpha=0.6)
+axp.plot(aiaa_pos23599_x1, ansi_pos23599_yy, c='g', marker='', ls='-', alpha=0.6)
+axp.plot(aiaa_pos23599_x1, ansi_pos23599_yz, c='m', marker='', ls='-', alpha=0.6)
+
+axp.set_xticks(xticksmin, True);
+axp.set_xticks(xticksmaj, False);
+axp.autoscale(True, 'y', None)
+axp.grid(which='major', axis='both', ls='dashed', alpha=0.7)
+axp.grid(which='minor', axis='x', ls='dotted', alpha=0.3)
+axp.set_xlabel('Time from epoch, min')
+axp.set_ylabel('Position components, km')
+fig.set_tight_layout({'pad':0.0, 'w_pad':0.1, 'h_pad':0.1})
+
+# Lyddane Choice Modification for Satellite 14128 #############################
+
+incl_bar = []
+aiaa_14128_t = []
+aiaa_14128_i = []
+ansi_14128_i = []
+dpy_14128 = []
+dpz_14128 = []
+
+for time in aiaa_ver_sats['14128'][2].keys():
+  incl_bar.append(11.4592)
+  aiaa_14128_t.append(time)
+  aiaa_14128_i.append(aiaa_ver_sats['14128'][2][time][5])
+  ansi_14128_i.append(ansi_ver_sats['14128'][2][time][5])
+  dpy_14128.append(aiaa_ver_sats['14128'][2][time][1] - ansi_ver_sats['14128'][2][time][1])
+  dpz_14128.append(aiaa_ver_sats['14128'][2][time][2] - ansi_ver_sats['14128'][2][time][2])
+
+fig, (axi, axp) = plt.subplots(2, 1, sharex=True)
+fig.canvas.set_window_title('Lyddane Choice for 14128') 
+
+xmin = 0
+xmax = 2880
+xticksmin = range(xmin, xmax + 1, 60)
+xticksmaj = range(xmin, xmax + 1, 240)
+
+axi.plot(aiaa_14128_t, incl_bar, c='k', marker='', ls='-', lw=0.5)
+
+axi.plot(aiaa_14128_t, aiaa_14128_i, c='k', marker='', ls=':')
+axi.plot(aiaa_14128_t, ansi_14128_i, c='b', marker='', ls='-', alpha=0.6)
+
+axp.plot(aiaa_14128_t, dpy_14128, c='g', marker='', ls='-')
+axp.plot(aiaa_14128_t, dpz_14128, c='m', marker='', ls='-')
+
+axi.set_xlim(xmin, xmax)
+axi.set_xticks(xticksmin, True);
+axi.set_xticks(xticksmaj, False);
+axi.autoscale(True, 'y', None)
+axi.grid(which='major', axis='both', ls='dashed', alpha=0.7)
+axi.grid(which='minor', axis='x', ls='dotted', alpha=0.3)
+axi.set_title('Lyddane Choice for 14128, AIAA (black dashed) vs ANSI (color)')
+axi.set_ylabel('Inclination, deg')
+
+axp.set_xticks(xticksmin, True);
+axp.set_xticks(xticksmaj, False);
+axp.set_ylim(y2min, y2max)
+axp.set_yticks(y2ticksmin, True);
+axp.set_yticks(y2ticksmaj, False);
+axp.grid(which='major', axis='both', ls='dashed', alpha=0.7)
+axp.grid(which='minor', axis='x', ls='dotted', alpha=0.3)
+axp.set_xlabel('Time from epoch, min')
+axp.set_ylabel('Position difference, m')
+fig.set_tight_layout({'pad':0.0, 'w_pad':0.1, 'h_pad':0.1}) 
 
 plt.show()
